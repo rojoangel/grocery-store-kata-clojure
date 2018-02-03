45f0b5a0f4319eb713cdb4b39c78bb696fa86fda
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-03 22:12:07 +0100

    fix broken tests

--- test/grocery_store/core_test.clj
+++ test/grocery_store/core_test.clj
@@ -20,26 +20,32 @@
    "bread" "wheat and pasta"
    "eggs" "animalic"})
 
-(def categories-map
-  (into {} (for [category categories] (vector (second category) 0))))
+(defn- item->category [item]
+  (get categories (some #(re-find (re-pattern (key %)) item) categories)))
 
-(defn- record->category [record]
-  (get categories (some #(re-find (re-pattern (key %)) (first record)) categories)))
+(defn- item [record]
+  (first record))
 
-(defn- category-and-total [record]
-  (vector (record->category record) (last record)))
+(defn- quantity [record]
+  (last record))
 
-(defn- add-quantity-to-category [categories-map category-and-total]
-  (update categories-map (first category-and-total) + (last category-and-total)))
+(defn- category [record]
+  (first record))
+
+(defn- category-and-quantity [record]
+  (vector (item->category (item record)) (quantity record)))
+
+(defn- add-quantity-to-category [categories-map category-and-quantity]
+  (update categories-map (category category-and-quantity) (fnil + 0) (quantity category-and-quantity)))
 
 (defn categories-total [record-of-sales]
   (reduce
     add-quantity-to-category
-    categories-map
-    (map category-and-total record-of-sales)))
+    {}
+    (map category-and-quantity record-of-sales)))
 
 (deftest categories-total-test
   (testing "Category total is properly calculated"
-    (is (= {"wheat and pasta" 4}) (categories-total [["bread" 1 2]]))
-    (is (= {"wheat and pasta" 4}) (categories-total [["bread" 1 2] ["bread" 1 2]]))
-    (is (= {"wheat and pasta" 2 "animalic" 3}) (categories-total [["bread" 1 2] ["12-pack of eggs" 1 3]]))))
+    (is (= {"wheat and pasta" 2} (categories-total [["bread" 1 2]])))
+    (is (= {"wheat and pasta" 4} (categories-total [["bread" 1 2] ["bread" 1 2]])))
+    (is (= {"wheat and pasta" 2 "animalic" 3} (categories-total [["bread" 1 2] ["12-pack of eggs" 1 3]])))))
